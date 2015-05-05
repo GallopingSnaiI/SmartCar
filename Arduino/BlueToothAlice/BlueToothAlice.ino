4e314379fb4e49e3fa881b162eca64078f1e070f
GallopingSnaiI <kevin.nie@hotmail.sg>
2015-05-05 11:31:49 +0200

    comments add

--- Arduino/BlueToothAlice/BlueToothAlice.ino
+++ Arduino/BlueToothAlice/BlueToothAlice.ino
@@ -4,48 +4,53 @@
 
 SoftwareSerial bluetooth(50,51); 
 Smartcar alice;
-int arraylength;
+//int arraylength;
 
 void setup() 
 {
- // put your setup code here, to run once:
  alice.begin();
  Serial2.begin(9600);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
   if(Serial2.available() > 0)
-  {
+  { //initialize a queue as accommodation
     int arraylength = Serial2.readStringUntil('!').toInt();
     String queue[arraylength];
-    
     int i = 0;
-    while(i<arraylength)
+    
+    //store instructions in queue
+    while(Serial2.available() > 0 && i<arraylength)
     {
-      if(Serial2.available() > 0){
+      //if(){
         queue[i] = Serial2.readStringUntil('*');
         i++;
-      }
+      //}
     }
-    for(int i = 0; i<arraylength;i++)
+    for(int i = 0; i<arraylength; i++)
     {
         String instr = "";
         String parameter = "";
         boolean spaceFound = false;
-        for(int j = 0; j<queue[i].length();j++){
+        
+        //interpret instruction
+        for(int j = 0; j<queue[i].length(); j++)
+        {
           if(queue[i].charAt(j)==' '){
             spaceFound = true;
           }else if(spaceFound){
             parameter += queue[i].charAt(j);
           }else{
             instr += queue[i].charAt(j); 
-          }     
-      }   
+          }
+        }   
+      
+      //print instruction
       Serial2.println(instr);
       int value = parameter.toInt();
       
+      //excute instruction
       if(instr.equals("goForward")){
           alice.goForward(value);
       }else if(instr=="rotateClockwise"){
@@ -58,7 +63,3 @@ void loop()
     }
   }
 }
-
-
-
-
