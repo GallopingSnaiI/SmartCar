296da581e8f1bf6cf6975416d84da858e57fccd0
GallopingSnaiI <kevin.nie@hotmail.sg>
2015-05-21 13:39:22 +0200

    re-commended

--- Arduino/manualControlAlice/manualControlAlice.ino
+++ Arduino/manualControlAlice/manualControlAlice.ino
@@ -12,21 +12,22 @@ Sonar sonar;
 const int trig_pin = 43;
 const int echo_pin = 42;
 
+//Global variables
 volatile int counter;
 boolean obstacle;
 int index;
 String mode;
   
 void setup(){
-   Serial2.begin(9600);
-   
+   Serial2.begin(9600);   
    alice.begin();
    sonar.attach(trig_pin, echo_pin);
    encoder.attach(odo_pin);
 }
 
 void loop(){
-  //Serial2.println("Car waiting");
+  
+  //Idle state.
   while(true){    
     if(Serial2.available() > 0){
       if (Serial2.peek() == '$'){
@@ -39,6 +40,8 @@ void loop(){
       }
     }
   }
+  
+  //switch between manual mode and auto mode according to data received.
   if(mode.equals("Auto")){
     autoMode();
   }else if(mode.equals("Manual")){
@@ -46,17 +49,21 @@ void loop(){
   }   
 }
 
-void manualMode() {
+//manual mode
+void manualMode(){
+  
   alice.setAutomationRotationSpeed(150);
-  //Serial2.println("Manual mode entered, waiting for manual control.");
+  
   String instr;
   
+  //main loop
   while(true){    
+    
     if(Serial2.available() > 0){
-        
+      
+      //validate exit condition and filter out trash information  
       if(Serial2.peek() == '@'){
           Serial2.readStringUntil('@');
-          //Serial2.println("Manual mode end, turn back into idle mode.");
           break;
         }else if(Serial2.peek() == '$'){
           Serial2.readStringUntil('$');
@@ -66,35 +73,34 @@ void manualMode() {
         }
       }
       
+      //execute manual commandes.
       if(instr.equals("goForward")){
-        //Serial2.println("Going forward.");
         alice.goForward();
       }else if(instr.equals("goBackward")){
-       // Serial2.println("Going backward.");
         alice.goBackward();
       }else if(instr.equals("rotateClockwise")){
-        //Serial2.println("Rotating clockwise.");
         alice.rotateClockwise(1);
       }else if(instr.equals("rotateCounterClockwise")){
-        //Serial2.println("Rotating counterclockwise");
         alice.rotateCounterClockwise(1);
       }else{
-        //Serial2.println("Stop.");
         alice.stop();
       }
     }
   }
-  
+
+//path following mode  
 void autoMode(){
+  
   alice.setAutomationRotationSpeed(100);
-  //Serial2.println("Auto mode entered.");
+  
   if(Serial2.available() > 0){ 
+    
     //initialize a queue as accommodation
     int arraylength = Serial2.readStringUntil('!').toInt();
     String queue[arraylength];
     int k = 0;
         
-    //store instructions in queue
+    //store instructions in array
     while(k<arraylength){
       if(Serial2.available() > 0){
         queue[k] = Serial2.readStringUntil('*');
@@ -102,36 +108,39 @@ void autoMode(){
       }
     }
     
-    //interpret and excute instructions
+    //intepret and excute instructions
     for(index = 0; index<arraylength && !obstacle; index++){
-        String instr = "";
-        String parameter = "";
-        boolean spaceFound = false;
-        
-        for(int j = 0; j<queue[index].length(); j++){
-          if(queue[index].charAt(j)==' '){
-            spaceFound = true;
-          }else if(spaceFound){
-            parameter += queue[index].charAt(j);
-          }else{
-            instr += queue[index].charAt(j); 
-          }
-        }
       
-      //excute instruction
+      //intrpret  
+      String instr = "";
+      String parameter = "";
+      boolean spaceFound = false;
+      
+      for(int j = 0; j<queue[index].length(); j++){
+        if(queue[index].charAt(j)==' '){
+          spaceFound = true;
+        }else if(spaceFound){
+          parameter += queue[index].charAt(j);
+        }else{
+          instr += queue[index].charAt(j);
+        }
+      }
+      
+      //excute
       int value = parameter.toInt();
       
       if(instr.equals("goForward")){
-          goForwardSafe(value);
+        goForwardSafe(value);
       }else if(instr=="rotateClockwise"){
-          alice.rotateClockwise(value);
+        alice.rotateClockwise(value);
       }else if(instr=="rotateCounterClockwise"){
-          alice.rotateCounterClockwise(value);
+        alice.rotateCounterClockwise(value);
       }
     }
   }
 }
 
+//go forward with detect obstacle function working
 void goForwardSafe(int desiredDistance){
   encoder.begin();
   
@@ -142,6 +151,7 @@ void goForwardSafe(int desiredDistance){
   brake();
 }
 
+//detect obstacle function.
 boolean isFrontClear(){
   int distance = sonar.getDistance();
   
@@ -161,6 +171,7 @@ boolean isFrontClear(){
   return true;
 }
 
+//brake safe in front of the obstacle.
 void brake(){
   alice.stop();
   delay(50);
@@ -169,6 +180,7 @@ void brake(){
   alice.stop();
 }
 
+//reset variables.
 void reset(){
   counter = 0;
   obstacle = false;
